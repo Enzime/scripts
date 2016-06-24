382e2db21ab72ea3f270887bcf446963339c345e
Michael Hoang <enzime@users.noreply.github.com>
2016-06-24 22:01:41 +1000

    Use ~/.api_keys and fix HTTP regex

--- bin/puu.sh
+++ bin/puu.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-# Uncommend following line if you would like to add api key in this file
-PUUSH_API_KEY=""
+source ~/.api_keys
 
 if [ -z "$PUUSH_API_KEY" ]
 then
-  echo "Set the variable PUUSH_API_KEY in $0 or with 'export PUUSH_API_KEY=\"apiKeyHere\""
-  exit 1
+    echo "Add 'export PUUSH_API_KEY=\"apiKeyHere\"' to ~/.api_keys"
+    exit 1
 elif [ -z "$1" ]
 then
-  echo "Specify a file to be uploaded"
-  exit 2
+    echo "Specify a file to be uploaded"
+    exit 2
 elif ! [ -f "$1" -a -r "$1" ]
 then
-  echo "File '$1' is not valid (it is not a file or it is not readable)"
-  exit 3
+    echo "File '$1' is not valid (it is not a file or it is not readable)"
+    exit 3
 fi
 
 curl "https://puush.me/api/up" -# -F "k=$PUUSH_API_KEY" -F "z=poop" -F "f=@$1" | sed -E 's/^.+,(.+),.+,.+$/\1\n/'
